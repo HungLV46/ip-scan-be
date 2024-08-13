d4f33cd0d8e007c1e01a7c6093514bb20afd019d
duonghb53 <duonghb53@gmail.com>
2024-08-13 10:59:26 +0700

    change document field and remove unused

--- src/elastic-search/indexes/products/base/index.ts
+++ src/elastic-search/indexes/products/base/index.ts
@@ -2,19 +2,29 @@ import { BuildDocumentData, DocumentBuilder } from './../../base/index';
 import { BaseDocument } from 'elastic-search/indexes/base';
 
 export interface ProductDocument extends BaseDocument {
-  product_id: number; // id in postgres database
   name: string;
   category: string;
   description: string;
   avatar_img: string;
   banner_img: string;
   metadata: JSON;
-  owner: string;
+  owner: UserDocument;
   featured_at: Date;
   updated_at: Date;
 
   collections: CollectionDocument[];
   attributes: ProductAttributeDocument[];
+}
+
+interface UserDocument {
+  id: number;
+  name: string;
+  bio: string;
+  email: string;
+  wallet_address: string;
+  avatar_img: string;
+  banner_img: string;
+  additional_info: JSON;
 }
 
 interface CollectionDocument {
@@ -31,7 +41,6 @@ interface ProductAttributeDocument {
 }
 
 export interface BuildProductDocumentData extends BuildDocumentData {
-  product_id: number; // id in postgres database
   chain_id?: number;
   name: string;
   category: string;
@@ -39,7 +48,7 @@ export interface BuildProductDocumentData extends BuildDocumentData {
   avatar_img: string;
   banner_img: string;
   metadata: JSON;
-  owner: string;
+  owner: UserDocument;
   featured_at: Date;
   updated_at: Date;
 
@@ -62,7 +71,7 @@ export class ProductDocumentBuilder extends DocumentBuilder {
       console.error(
         'ProductDocumentBuilder',
         JSON.stringify({
-          message: `buildDocument Error. product id = ${data.product_id}, error=${error}`,
+          message: `buildDocument Error. product id = ${data.id}, error=${error}`,
           data,
           error,
         }),
--- src/elastic-search/indexes/products/utils.ts
+++ src/elastic-search/indexes/products/utils.ts
@@ -3,31 +3,5 @@ import { BuildProductDocumentData } from './base';
 export const productToProductDocumentData = (
   product: any,
 ): BuildProductDocumentData => {
-  const document = {
-    id: product.id,
-    product_id: product.id,
-    name: product.name,
-    category: product.category,
-    description: product.description,
-    avatar_img: product.avatar_img,
-    banner_img: product.banner_img,
-    metadata: product.metadata,
-    owner: product.owner.name,
-    featured_at: product.featured_at,
-    updated_at: product.updated_at,
-
-    collections: product.collections.map((collection: any) => ({
-      id: collection.id,
-      name: collection.name,
-      chain_id: collection.chain_id,
-      contract_address: collection.contract_address,
-    })),
-    attributes: product.attributes.map((attribute: any) => ({
-      id: attribute.id,
-      name: attribute.name,
-      value: attribute.value,
-    })),
-  } as BuildProductDocumentData;
-
-  return document;
+  return product as BuildProductDocumentData;
 };
